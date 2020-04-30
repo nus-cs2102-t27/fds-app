9e47459d034a246a7b7c1529734272fb893de7c8
yhtingg <huitingxd@gmail.com>
2020-04-30 16:43:23 +0800

    Add food for testing purposes

---
+++ database/Food.sql
@@ -0,0 +1,7 @@
+insert into food(rid, name, category, price, food_limit) values (1, 'Hawaiian Pizza', 'Pizza', 20, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Meat Lovers Pizza', 'Pizza', 20, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Mushroom Pizza', 'Pizza', 18, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Dessert Pizza', 'Pizza', 18, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Drumlets 6pc', 'Sides', 10, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Wings 6pc', 'Pizza', 10, 100);
+insert into food(rid, name, category, price, food_limit) values (1, 'Coca-Cola', 'Drinks', 2.70, 100);
