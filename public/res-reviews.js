fa62ec5c02c4e0d59389ed61c7c198f1be46b62c
yhtingg <huitingxd@gmail.com>
2020-05-06 05:42:21 +0800

    Add reviews for restaurants

--- public/res-reviews.js
+++ public/res-reviews.js
@@ -1,23 +1,7 @@
-const rid = (new URL(location.href)).searchParams.get("rid");
-
-$("#res-name").load(`/api/res/${rid}/name`);
-
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-function formatDate(date) {
-    var year = date.getFullYear(),
-        month = monthNames[date.getMonth()];
-        day = date.getDate();
-
-    return `${day} ${month} ${year}`;
-}
-
-$.ajax(`/api/res/${rid}/reviews`).done(orders => {
+$.ajax("/api/res/resreviews").done(orders => {
     orders.forEach(order => {
         $("#reviews").append(`
             <tr>
-              <td>${formatDate(new Date(order.order_time))}</td>
               <td>${order.name}</td>
               <td>
                 <table class="table-borderless">
--- public/restaurants.js
+++ public/restaurants.js
@@ -1,6 +1,8 @@
 $.ajax("/api/res/all", { dataType: "json" }).done(res => {
     res.forEach(restaurant => {
-        $("#restaurants").append('<a href="/app/menu.html?rid='
-        + restaurant.rid + '">' + restaurant.name + '</a><br>')
+        $("#restaurants").append(`
+            <a href="/app/menu.html?rid=${restaurant.rid}">${restaurant.name}</a>
+            <a href="/app/reviews.html?rid=${restaurant.rid}">Reviews</a>
+        `);
     })
 })
