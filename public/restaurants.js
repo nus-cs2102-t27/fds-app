8745544648961c599e74c1a201e14e2631b79ee7
yhtingg <huitingxd@gmail.com>
2020-05-07 05:49:07 +0800

    Fix category browsing bug

--- public/restaurants.js
+++ public/restaurants.js
@@ -3,17 +3,23 @@ $.ajax("/api/res/categories").done(categories => {
     $("#category").append(`
       <option value='${category.category}'>${category.category}</option>
     `);
-  })
-})
+  });
+});
+
+$.ajax("/api/res/all/").done(res => populateRestaurants(res));
 
 $("#category").change(() => {
     $.ajax("/api/res/cat/" + $("#category").val()).done(res => {
         $("#restaurants").empty();
-        res.forEach(restaurant => {
-            $("#restaurants").append(`
-                <a href="/app/menu.html?rid=${restaurant.rid}">${restaurant.name}</a>
-                <a href="/app/reviews.html?rid=${restaurant.rid}">Reviews</a>
-           `);
-        })
-    })
-})
+        populateRestaurants(res);
+    });
+});
+
+function populateRestaurants(res) {
+  res.forEach(restaurant => {
+      $("#restaurants").append(`
+          <a href="/app/menu.html?rid=${restaurant.rid}">${restaurant.name}</a>
+          <a href="/app/reviews.html?rid=${restaurant.rid}">Reviews</a>
+     `);
+  });
+}
